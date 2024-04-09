7d82fe7a8a8cb1c6fa7bb20f5c11e09fb38043b2
JiaX-TCS <jiaxie@student.ethz.ch>
2024-04-10 00:36:13 +0200

    add fully connected graphs

--- grit/encoder/equivstable_laplace_pos_encoder.py
+++ grit/encoder/equivstable_laplace_pos_encoder.py
@@ -4,43 +4,6 @@ from torch_geometric.graphgym.config import cfg
 from torch_geometric.graphgym.register import register_node_encoder
 from torch_scatter import scatter
 import torch_sparse
-def full_edge_index(edge_index, batch=None):
-    """
-    Retunr the Full batched sparse adjacency matrices given by edge indices.
-    Returns batched sparse adjacency matrices with exactly those edges that
-    are not in the input `edge_index` while ignoring self-loops.
-    Implementation inspired by `torch_geometric.utils.to_dense_adj`
-    Args:
-        edge_index: The edge indices.
-        batch: Batch vector, which assigns each node to a specific example.
-        batch: Batch vector, which assigns each node to a specific example.
-    Returns:
-        Complementary edge index.
-    """
-
-    if batch is None:
-        batch = edge_index.new_zeros(edge_index.max().item() + 1)
-
-    batch_size = batch.max().item() + 1
-    one = batch.new_ones(batch.size(0))
-    num_nodes = scatter(one, batch,
-                        dim=0, dim_size=batch_size, reduce='add')
-    cum_nodes = torch.cat([batch.new_zeros(1), num_nodes.cumsum(dim=0)])
-
-    negative_index_list = []
-    for i in range(batch_size):
-        n = num_nodes[i].item()
-        size = [n, n]
-        adj = torch.ones(size, dtype=torch.short,
-                         device=edge_index.device)
-
-        adj = adj.view(size)
-        _edge_index = adj.nonzero(as_tuple=False).t().contiguous()
-        # _edge_index, _ = remove_self_loops(_edge_index)
-        negative_index_list.append(_edge_index + cum_nodes[i])
-
-    edge_index_full = torch.cat(negative_index_list, dim=1).contiguous()
-    return edge_index_full
 
 
 @register_node_encoder('EquivStableLapPE')
